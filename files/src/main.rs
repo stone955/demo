0b2ace7c7b5e0b3123f7f392f7c669b650db6fcd
stone <335612970@qq.com>
2020-07-31 19:12:20 +0800

    file write

--- files/src/main.rs
+++ files/src/main.rs
@@ -2,12 +2,20 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let mut file = File::open("info.txt").expect("Can not open file!");
+    // 从 info.txt 读
+    let mut input_file = File::open("input.txt").expect("Can not open file!");
 
     let mut contents = String::new();
 
-    file.read_to_string(&mut contents)
+    input_file
+        .read_to_string(&mut contents)
         .expect("Can not read the file!");
 
-    println!("File contects:\n\n{}", contents);
+    println!("Info contents:\n\n{}", contents);
+
+    // 将读取的内容写到 output.txt
+    let mut output_file = File::create("output.txt").expect("Can not create file!");
+    output_file
+        .write_all(&mut contents.as_bytes())
+        .expect("Can not write file!");
 }
