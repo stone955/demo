63b5b74635171a7e3fa77a0ceb54f318656b0fc2
stone <335612970@qq.com>
2020-07-31 19:36:17 +0800

    match

--- statement/src/main.rs
+++ statement/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // if... else if... else...
     let n = 50;
     if n == 30 {
         println!("The number is equals 30.");
@@ -6,5 +7,16 @@ fn main() {
         println!("The number is less than 30.");
     } else {
         println!("The number is more than 30.");
+    }
+
+    // match(switch)
+
+    let age = 61;
+
+    match age {
+        0..=18 => println!("The age {} is juvenile!", age),
+        19..=40 => println!("The age {} is youth!", age),
+        41..=60 => println!("The age {} is middle-aged!", age),
+        _ => println!("The age {} is oldder!", age),
     }
 }
