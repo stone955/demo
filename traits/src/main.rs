fea97f546cd18cb5c21dba5bcaa9ad1ba6ff9cb9
stone <335612970@qq.com>
2020-07-31 19:25:17 +0800

    define trait

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -3,10 +3,36 @@ struct Person {
     age: u8,
 }
 
+trait Movable {
+    // run
+    fn run(&self);
+    // walk
+    fn walk(&self);
+}
+
 // Person implements trait ToString
 impl ToString for Person {
     fn to_string(&self) -> String {
         return format!("name: {}, age: {}", self.name, self.age);
+    }
+}
+
+// Person implements trait Movable
+impl Movable for Person {
+    fn run(&self) {
+        if self.age < 40 {
+            println!("I can run fastly!");
+        } else {
+            println!("I can run slowly!");
+        }
+    }
+
+    fn walk(&self) {
+        if self.age < 50 {
+            println!("I can walk fastly!");
+        } else {
+            println!("I can walk slowly!");
+        }
     }
 }
 
@@ -17,4 +43,20 @@ fn main() {
     };
 
     println!("Person {}", p.to_string());
+
+    // 接口实现
+    let younger = Person {
+        name: String::from("Younger"),
+        age: 35,
+    };
+
+    younger.run();
+    younger.walk();
+
+    let oldder = Person {
+        name: String::from("Older"),
+        age: 50,
+    };
+    oldder.run();
+    oldder.walk();
 }
