f9d7eb8bdc3bc032379e4ecb803dacf1efa37aa7
stone <335612970@qq.com>
2020-08-07 01:41:35 +0800

    string replace

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -10,8 +10,23 @@ fn main() {
         println!("{}", token);
     }
     // contains
-    println!("Does the string contains 'stone'? {}", str.contains("stone"));
+    println!(
+        "Does the string contains 'stone'? {}",
+        str.contains("stone")
+    );
     // append
     str.push_str("Welcome to learn rust strings.");
     println!("{}", str);
+
+    // string method
+    // Replace
+    let before_str = String::from("Java is great!");
+    let after_str = before_str.replace("Java", "Golang");
+    println!(
+        "Before replace: {}. After replace: {}",
+        before_str, after_str
+    );
+
+    // Lines
+    
 }
