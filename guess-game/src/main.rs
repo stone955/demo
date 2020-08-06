26863320dee20ec39367585f1f9bc755194cd2bd
stone <335612970@qq.com>
2020-08-06 16:49:31 +0800

    guess game loop

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -1,3 +1,53 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // 0. Read from user input
+    /*
+    println!("Guess the number!");
+
+    println!("Please inpput your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("fatal to read line");
+
+    println!("You guessed: {}", guess);
+    */
+
+    // 1. Generate a secret number
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(0, 100);
+
+    // 可以用二分查找猜数字，最多logN次就能猜到
+    loop {
+        println!("Please inpput your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("fatal to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small!");
+            }
+            Ordering::Greater => {
+                println!("Too big!");
+            }
+            Ordering::Equal => {
+                println!("Yeah! You win!");
+                break;
+            }
+        }
+    }
 }
