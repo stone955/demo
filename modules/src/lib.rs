20aa31c54b2b339d1a311e9432e318c99d21b73a
stone <335612970@qq.com>
2020-08-13 15:12:18 +0800

    module

--- modules/src/lib.rs
+++ modules/src/lib.rs
@@ -1,15 +1,23 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
+mod back_of_house;
 
-    mod serving {
-        fn take_order() {}
+fn serve_order() {}
 
-        fn serve_order() {}
+use front_of_house::hosting;
 
-        fn take_payment() {}
-    }
+pub fn eat_at_restaurant() {
+    // Absolute path
+    hosting::add_to_waitlist();
+
+    // Relattive path
+    hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please!", meal.toast);
+
+    let soup_order = back_of_house::Appetizer::Soup;
+    let salad_order = back_of_house::Appetizer::Salad;
 }
