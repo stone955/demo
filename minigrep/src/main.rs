4d40bac125c6f1deaa4735222a0e376c74f267e7
stone <335612970@qq.com>
2020-08-18 19:26:46 +0800

    3.Separating concerns: main.rs handles running the program, and lib.rs handles all the logic of the task at hand

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -10,10 +10,7 @@ fn main() {
     println!("The Args: {:?}", args);
 
     // [Output] The Args: ["target\\debug\\minigrep.exe", "searchstring", "example-filename.txt"]
-
-    // saving the argument values in variables
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_arguments(&args);
 
     println!("Searching for {}, in file {}", query, filename);
 
@@ -24,4 +21,11 @@ fn main() {
 
     // This pattern is about separating concerns: main.rs handles running the program,
     // and lib.rs handles all the logic of the task at hand
+}
+
+fn parse_arguments(args: &[String]) -> (&str, &str) {
+    // saving the argument values in variables
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
 }
