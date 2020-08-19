bd7ef28d8d1df89bfcaedbb7217375b951dd9b3c
stone <335612970@qq.com>
2020-08-19 11:20:51 +0800

    Grouping Configuration Values

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -10,12 +10,17 @@ fn main() {
     println!("The Args: {:?}", args);
 
     // [Output] The Args: ["target\\debug\\minigrep.exe", "searchstring", "example-filename.txt"]
-    let (query, filename) = parse_arguments(&args);
 
-    println!("Searching for {}, in file {}", query, filename);
+    // let (query, filename) = parse_arguments(&args);
+    // println!("Searching for {}, in file {}", query, filename);
+
+
+    // Grouping Configuration Values
+    let parameter = parse_parameter(&args);
+    println!("Searching for {}, in file {}", parameter.query, parameter.filename);
 
     // read file
-    let contents = fs::read_to_string(filename).expect("Failed to read the file");
+    let contents = fs::read_to_string(parameter.filename).expect("Failed to read the file");
 
     println!("Text: \n{}", contents);
 
@@ -23,9 +28,24 @@ fn main() {
     // and lib.rs handles all the logic of the task at hand
 }
 
-fn parse_arguments(args: &[String]) -> (&str, &str) {
+struct Parameter {
+    query: String,
+    filename: String,
+}
+
+// fn parse_arguments(args: &[String]) -> (&str, &str) {
+//     // saving the argument values in variables
+//     let query = &args[1];
+//     let filename = &args[2];
+//     (query, filename)
+// }
+
+fn parse_parameter(args: &[String]) -> Parameter {
     // saving the argument values in variables
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Parameter {
+        query,
+        filename,
+    }
 }
