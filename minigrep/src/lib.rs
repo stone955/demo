7f6b0257e7709f95c7b70558309c8aae8e377b13
stone <335612970@qq.com>
2020-08-19 15:40:48 +0800

    Test Driver Deployment

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,7 +22,36 @@ impl Parameter {
 }
 
 pub fn run(parameter: Parameter) -> Result<(), Box<dyn Error>> {
+    let query = parameter.query;
     let contents = fs::read_to_string(parameter.filename)?;
-    println!("Text: \n{}", contents);
+    for line in search(&query, &contents) {
+        println!("{},", line);
+    }
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
