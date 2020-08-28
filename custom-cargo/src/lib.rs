4a444035beb79a4b9ae63e3686494b81b655cff3
stone <335612970@qq.com>
2020-08-28 16:33:56 +0800

    More about Cargo and Crates.io

---
+++ custom-cargo/src/lib.rs
@@ -0,0 +1,22 @@
+//! # Custom-Cargo
+//!
+//! `custom-cargo` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+/// Increment one to the number given.
+///
+/// # Examples
+/// ```
+/// fn main() {
+///     println!("Hello, world!");
+/// }
+/// ```
+/// # Panics
+///
+/// # Errors
+///
+/// # Safety
+///
+pub fn increment(x: i32) -> i32 {
+    x + 1
+}
